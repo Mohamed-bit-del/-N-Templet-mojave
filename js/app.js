37ae02b78a09b8e9d6e2210068e2a76262bec9ef
Mohamed <tmido2890@gmail.com>
2021-04-23 01:02:28 +0200

    Creat New Tamplate

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById('mobile__screen').style.left = '0';
+  document.getElementById('mobile').style.left = '0';
+  document.getElementById('mobil__close').style.left = '820px';
+}
+
+function closeNav() {
+  document.getElementById('mobile__screen').style.left = '-1030px';
+  document.getElementById('mobile').style.left = '-1030px';
+  document.getElementById('mobil__close').style.left = '-1030px';
+}
