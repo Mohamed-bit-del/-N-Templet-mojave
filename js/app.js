68b3051a3fd735f3e6f9e61505913ef2c6dc3b16
Mohamed <tmido2890@gmail.com>
2021-05-06 03:05:35 +0200

    chang style sass

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function openNav() {
   document.getElementById('mobile__screen').style.left = '0';
   document.getElementById('mobile').style.left = '0';
-  document.getElementById('mobil__close').style.left = '820px';
+  document.getElementById('mobil__close').style.left = '75vw';
 }
 
 function closeNav() {
